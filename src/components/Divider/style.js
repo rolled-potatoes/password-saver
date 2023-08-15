0c9707db8b7c7b322001536d0cc32696fdc53d86
rolled-potatoes <keep4404@gmail.com>
2023-08-15 14:50:34 +0900

    fix: divider color 수정

--- src/components/Divider/style.js
+++ src/components/Divider/style.js
@@ -3,6 +3,5 @@ import styled from 'styled-components';
 export const Hr = styled.hr`
   margin: 0;
   padding: 0;
-  background-color: var(--divider);
-  border-color: var(--divider);
+  border: 0.5px solid var(--divider);
 `;
